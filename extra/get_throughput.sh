eb60f8b94e8fa168e8366186930fc8080014efe5
Alessandro Toppi <atoppi@meetecho.com>
2023-07-11 14:31:16 +0200

    Fix bash script for avg throughput evaluation

--- extra/get_throughput.sh
+++ extra/get_throughput.sh
@@ -7,9 +7,8 @@ PCAP_FILE_PATH=$(echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")")
 #PROFILE=$(basename $(dirname "$PCAP_FILE_PATH"))
 #STACK=$(basename $(dirname $(dirname "$PCAP_FILE_PATH")))
 
-SUMMARY=$(tshark -z conv,udp -r $PCAP_FILE_PATH -q | tail -2 | head -1 | tr -s ' ')
-SIZE_BYTES=$(echo $SUMMARY | awk '{ print $5toupper(substr($6,1,1)) }' | numfmt --from=auto)
-DURATION_SECS=$(echo $SUMMARY | awk '{ print $14 }')
+SIZE_BYTES=$(tshark -r $PCAP_FILE_PATH -q -z io,stat,0,"SUM(frame.len)frame.len && udp.srcport == 443" | tail -2 | head -1 | tr -s ' ' | awk '{ print $6 }')
+DURATION_SECS=$(tshark -r $PCAP_FILE_PATH -q -z conv,udp | tail -2 | head -1 | tr -s ' ' | awk '{ print $NF }')
 THPUT_MBPS=$(echo "scale=2; ((8*$SIZE_BYTES)/($DURATION_SECS))/1000000" | bc)
 
 echo $THPUT_MBPS
