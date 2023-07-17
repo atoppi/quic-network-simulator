6a6d376c392bffd7ea393a10d603d13f5daff0fb
Lorenzo Serranti <lorenzoserranti@hotmail.it>
2023-07-17 16:18:13 +0200

    Added code for creating csv file with time trend of rtt and cwnd

--- extra/get_stats.py
+++ extra/get_stats.py
@@ -3,6 +3,8 @@ import json
 import os
 import pathlib
 import sys
+import csv
+import datetime
 
 client_results_path = pathlib.Path(sys.argv[1]).absolute()
 server_results_path = pathlib.Path(sys.argv[2]).absolute()
@@ -30,6 +32,8 @@ samples_rtt = 0
 sum_rtt = 0
 client_name = ""
 server_name = ""
+
+output_csv_file = str(server_results_path) + "/" + datetime.datetime.now().strftime("server_rtt_cwnd_%Y-%m-%d_%H-%M-%S.csv")
 
 if (client_qlog_path.is_file()):
     with open(client_qlog_path) as file_client:
@@ -63,6 +67,14 @@ if (client_qlog_path.is_file()):
 
 if (server_qlog_path.is_file()):
     with open(server_qlog_path) as file_server:
+        writer = csv.writer(open(output_csv_file, "w"))
+        curr_rtt_ms = 0
+        curr_cwnd = 0
+        first_time = 0.0
+        curr_time = 0.0
+        # Print first row on csv
+        writer.writerow(["curr_time;curr_cwnd;curr_rtt_ms"])
+
         if server_qlog_path.suffix == '.qlog':
             data = json.load(file_server)
             is_picoquic = False
@@ -79,12 +91,33 @@ if (server_qlog_path.is_file()):
                     if "latest_rtt" in event[3]:
                         samples_rtt += 1
                         sum_rtt += event[3]["latest_rtt"]/1000
+                        curr_rtt_ms = event[3]["latest_rtt"]/1000 
+                    if "cwnd" in event[3]:
+                        curr_cwnd = event[3]["cwnd"]
+                    if ("cwnd" in event[3] or "latest_rtt" in event[3]):
+                        if first_time == 0:
+                            first_time = event[0]
+                        curr_time = (event[0] - first_time) / 1000000
+                        curr_row = f"{curr_time:.6f};{curr_cwnd};{curr_rtt_ms:.3f}"
+                        # Print curr_time, curr_cwnd, curr_rtt_ms on csv
+                        writer.writerow([curr_row])
                 else:
                     if "name" in event and "packet_sent" in event["name"]:
                         sent_count += 1
                     if "data" in event and "latest_rtt" in event["data"]:
                         samples_rtt += 1
                         sum_rtt += event["data"]["latest_rtt"]
+                        curr_rtt_ms = event["data"]["latest_rtt"] 
+                    if "data" in event and "cwnd" in event["data"]:
+                        curr_cwnd = event["data"]["cwnd"] 
+                    if "time" in event and ("cwnd" in event["data"] or "latest_rtt" in event["data"]):
+                        if first_time == 0:
+                            first_time = event["time"]
+                        curr_time = (event["time"] - first_time) / 1000
+                        curr_row = f"{curr_time:.6f};{curr_cwnd};{curr_rtt_ms:.3f}"
+                        # Print curr_time, curr_cwnd, curr_rtt_ms on csv
+                        writer.writerow([curr_row])
+                
         elif server_qlog_path.suffix == '.sqlog':
             while True:
                 line = file_server.readline().strip()
@@ -99,6 +132,16 @@ if (server_qlog_path.is_file()):
                 if "data" in event and "latest_rtt" in event["data"]:
                     samples_rtt += 1
                     sum_rtt += event["data"]["latest_rtt"]
+                    curr_rtt_ms = event["data"]["latest_rtt"] 
+                if "data" in event and "congestion_window" in event["data"]:
+                    curr_cwnd = event["data"]["congestion_window"] 
+                if "data" in event and ("congestion_window" in event["data"] or "latest_rtt" in event["data"]):
+                    if first_time == 0:
+                            first_time = event["time"]
+                    curr_time = (event["time"] - first_time) / 1000
+                    curr_row = f"{curr_time:.6f};{curr_cwnd};{curr_rtt_ms:.3f}"
+                    # Print curr_time, curr_cwnd, curr_rtt_ms on csv
+                    writer.writerow([curr_row])
 
 if (client_qlog_path.is_file() and server_qlog_path.is_file()):
     # Packet loss
